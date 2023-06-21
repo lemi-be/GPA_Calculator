aa4c54dfa6bb020df42fd08131b30ddc14a8014c
Lemi Bekele <lemibekele365@gmail.com>
2023-06-21 20:53:01 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -6,9 +6,16 @@ class SGrade{
 	int Id;
 	int CrHrs;
 	char CourseN[10];
+	int URgrade;
 	int Nofsubject;
+	int sum[];
 	public:
 		fstream f;
+		void gradingSY(){
+			int A = 1004;//grading ssystem
+		}
+		
+		
 		void input(){
 		f.open("data.txt", ios::in | ios::out | ios::app);
 		if(f.is_open()){	
@@ -20,9 +27,13 @@ class SGrade{
 			for (int i=0; i < Nofsubject; i++){
 				cout << "enter first three Letter of the " << i+1 <<" course" << endl;
 				cin >> CourseN;
+				cout << "What is your grade for the course" << endl;
+				cin >> URgrade;
 				cout << "enter credit hour of the course " << endl;
 				cin >> CrHrs;
-				f << "Course Name: " << CourseN << "\tCredit Hour: " <<CrHrs << endl;
+				f << "Course Name: " << CourseN <<  "\tGrade: " << URgrade <<  "\tCredit Hour: " <<CrHrs << endl;
+				sum = CrHrs + Grade;
+				 
 			}
 		f.close();	
 		}
@@ -30,11 +41,23 @@ class SGrade{
     		std::cout << "Error opening file";
 		}
 }
-	
+	void calculation(){ // this is not finished re run to know whats wrong 
+		int added=0;
+		f.open("data.txt", ios::in);
+		if(f.is_open()){
+			for(int i=0; i < Nofsubject; i++){
+				added = added + CrHrs ;
+			}
+			
+		}
+		cout << added;
+	}
 	
 }; SGrade g;
 
+
 int main(){
 	g.input();
+	g.calculation();
 	return 0;
 }
